f11714deb8328ca80980a1f15988089140ea234c
bryanerayner <bryanerayner@bryans-mbp.gateway.2wire.net>
2013-12-13 18:47:26 -0500

    Add Sandbox.test.js

---
+++ test/sandbox.tests.js
@@ -0,0 +1,23 @@
+(function(){
+	var Sandbox, chai, _;
+
+	chai = require("chai");
+
+	_ = require("lodash");
+
+	Sandbox = require("../lib/Sandbox.js").App.Sandbox;
+
+	chai.should();
+
+
+	describe("Sandbox Properties",function()
+	{
+		it("should ");
+
+
+	});
+
+
+
+
+}).call(this);
